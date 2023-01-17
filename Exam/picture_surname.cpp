014b918df04eb1970821d6dd8e815898e21a8d9d
Pavel7712 <113592852+Pavel7712@users.noreply.github.com>
2023-01-17 11:53:56 +0300

    Create picture_surname.cpp

---
+++ Exam/picture_surname.cpp
@@ -0,0 +1,9 @@
+#include "picture_surname.h"
+
+using namespace std;
+
+int main()
+{
+    Picture_Surname::file_name( R"(C:\Users\79967\Desktop\MosPolytech\Prog\C++\Murakhtanov_221-332\image\Source.bmp)");
+    Picture_Surname::file_copy();
+}
