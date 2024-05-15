725615008e20fbda20eda05d2a47a4a7f2dccb43
cvazy <player7712@mail.ru>
2024-05-15 14:11:09 +0300

    New project update

--- Marry/assets/scripts/sendForm.js
+++ Marry/assets/scripts/sendForm.js
@@ -1,8 +1,42 @@
 const mainForm = document.querySelector(".main_form");
 const modalWindow = document.querySelector(".modal");
 
+function serializeForm(formNode) {
+  const { elements } = formNode;
+
+  return Array.from(elements)
+    .filter((item) => !!item.name)
+    .map((element) => {
+      const { name, type } = element;
+      const value =
+        type === "checkbox" || type === "radio"
+          ? element.checked
+          : element.value;
+
+      return { name, value };
+    });
+}
+
+async function sendData(data) {
+  return await fetch("api/internal/form/send/", {
+    method: "POST",
+    headers: { "Content-Type": "multipart/form-data" },
+    body: data,
+  });
+}
+
+async function handleFormSubmit() {
+  const data = serializeForm(mainForm);
+  const response = await sendData(data);
+}
+
 mainForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
   modalWindow.style.zIndex = "9999";
   modalWindow.classList.add("modal_visible");
+
+  handleFormSubmit().then((status) => {
+    console.log(status);
+  });
 });
--- Marry/assets/scripts/visitorsRadio.js
+++ Marry/assets/scripts/visitorsRadio.js
@@ -1,9 +1,16 @@
 const visitorsRadios = document.querySelectorAll(".visited_checkbox");
 const indicateOfGuests = document.querySelector("#indicate_of_guests");
 const alcoholPreference = document.querySelector("#alcohol_preference");
+const alcoholCheckboxes = alcoholPreference.querySelectorAll(".form_radio");
 const togglePillColor = document.querySelector(".toggle-pill-color");
 const allergyBlock = document.querySelector("#allergyText");
 const musicContainer = document.querySelector(".music_container");
+const successRegisterText = document.querySelector(".success_register__text");
+
+const notVisitedText = "Благодарим за ответ!";
+
+const visitedText =
+  "Благодарим вас за заполнение анкеты! Мы обязательно возьмём в учёт ваши пожелания. Будем очень рады видеть вас на нашей свадьбе!";
 
 visitorsRadios.forEach((radio) => {
   radio.addEventListener("change", () => {
@@ -15,6 +22,8 @@ visitorsRadios.forEach((radio) => {
       radio.checked = true;
 
       setVisibilityFormBlocks(true);
+
+      successRegisterText.textContent = visitedText;
 
       if (radio.id === "visitor_custom") {
         indicateOfGuests.classList.remove("d-none-imp");
@@ -29,6 +38,8 @@ visitorsRadios.forEach((radio) => {
         setTimeout(() => {
           indicateOfGuests.classList.add("d-none-imp");
         }, 300);
+
+        successRegisterText.textContent = notVisitedText;
       } else {
         indicateOfGuests.classList.remove("active_indicate_of_guests");
 
@@ -53,3 +64,11 @@ function setVisibilityFormBlocks(visible) {
     musicContainer.classList.add("hidden_form_block");
   }
 }
+
+alcoholCheckboxes.forEach((block) => {
+  const circleSpan = block.querySelector(".circle_checkbox_span");
+
+  circleSpan.addEventListener("click", () => {
+    circleSpan.nextElementSibling.click();
+  });
+});
